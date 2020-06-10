f50b8bf1b1ebb9facc04f8d8554fd53ea8fa6143
GeovanniGR <geovannigutierrez77@gmail.com>
2020-06-09 19:59:43 -0600

    primer carga del proyecto

---
+++ ProyectoFinalAlgoritmos/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: geovanni
+ *
+ * Created on 9 de junio de 2020, 07:55 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
