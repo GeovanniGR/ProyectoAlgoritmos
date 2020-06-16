00a81c6162935c5af1bb567749675e198bbe7c33
Gerald Aguilar <geralaguilarbravo22@gmail.com>
2020-06-16 16:42:20 -0600

    struct Grafo

---
+++ ProyectoFinalAlgoritmos/Grafo.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Grafo.cpp
+ * Author: gerald
+ * 
+ * Created on 16 de junio de 2020, 04:29 PM
+ */
+
+#include "Grafo.h"
+
+Grafo::Grafo() {
+    p = NULL;
+}
+
+Grafo::Grafo(const Grafo& orig) {
+}
+
+Grafo::~Grafo() {
+}
+
